5dbb89ebb412aa01c68e3ab61d971dd32238225a
hoanghuydev <tranvohoanghuy12ab@gmail.com>
2024-11-03 14:29:32 +0700

    config : add dependency and design db

---
+++ src/main/java/com/app/rentconnect/RentConnectApplication.java
@@ -0,0 +1,13 @@
+package com.app.rentconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/rentconnect/RentConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.rentconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
