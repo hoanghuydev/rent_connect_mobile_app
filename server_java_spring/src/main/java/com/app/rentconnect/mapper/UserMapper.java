a9ab220bf82d388ec85c338e439c6a88ef8f1cc2
hoanghuydev <tranvohoanghuy12ab@gmail.com>
2024-11-10 13:30:42 +0700

    feat :register

--- server_java_spring/src/main/java/com/app/rentconnect/mapper/UserMapper.java
+++ server_java_spring/src/main/java/com/app/rentconnect/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.app.rentconnect.mapper;
 
+import com.app.rentconnect.dto.request.RegisterRequestDTO;
 import com.app.rentconnect.dto.request.UserRequestDTO;
 import com.app.rentconnect.entity.User;
 import org.mapstruct.Mapper;
@@ -8,4 +9,5 @@ import org.mapstruct.Mapper;
 public interface UserMapper {
     UserRequestDTO toRequestDTO(User user);
     User toEntity(UserRequestDTO userDTO);
+    User registerDTOtoEntity(RegisterRequestDTO registerRequestDTO);
 }
--- server_java_spring/src/main/java/com/app/rentconnect/service/UserService.java
+++ server_java_spring/src/main/java/com/app/rentconnect/service/UserService.java
@@ -2,6 +2,7 @@ package com.app.rentconnect.service;
 
 import com.app.rentconnect.dto.request.LoginRequestDTO;
 import com.app.rentconnect.dto.request.RegisterRequestDTO;
+import com.app.rentconnect.entity.User;
 import com.app.rentconnect.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -13,5 +14,12 @@ import org.springframework.stereotype.Service;
 @FieldDefaults(level= AccessLevel.PRIVATE, makeFinal = true)
 public class UserService {
     UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
 
 }
