2945d66dd1f09869a6790fa4fb9acdd05f1cebda
hoanghuydev <tranvohoanghuy12ab@gmail.com>
2025-01-10 16:07:49 +0700

    fix : asdfjasdkjhsd

--- server_java_spring/src/main/java/com/app/rentconnect/v1/configuration/SecurityConfig.java
+++ server_java_spring/src/main/java/com/app/rentconnect/v1/configuration/SecurityConfig.java
@@ -27,9 +27,9 @@ import javax.crypto.spec.SecretKeySpec;
 @EnableWebSecurity
 public class SecurityConfig {
     private final String[] PUBLIC_POST_ENDPOINT = {"/api/v1/auth/register","/api/v1/auth/verify","/api/v1/otp/send","/api/v1/auth/login","/api/v1/car/add"};
-    private final String[] PUBLIC_GET_ENDPOINT = {"/api/v1/auth/register","/api/v1/auth/login","/api/v1/user/{id}"};
+    private final String[] PUBLIC_GET_ENDPOINT = {"/api/v1/auth/register","/api/v1/auth/login","/api/v1/user/{id}","/api/v1/car/{carId}"};
     private final String[] ROLE_ADMIN_ENDPOINT = {};
-    private final String[] ROLE_OWNER_ENDPOINT = {"/api/v1/car/{carId}"};
+    private final String[] ROLE_OWNER_ENDPOINT = {};
     private final String[] ROLE_CUSTOMER_ENDPOINT = {};
     @Value("${jwt.secret}")
     private String jwtSecret;
