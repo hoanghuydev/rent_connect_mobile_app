80da0a04c6248796bece40d39a319fafcfebb96a
hoanghuydev <tranvohoanghuy12ab@gmail.com>
2025-01-16 19:42:20 +0700

    fix : error mapper car

--- server_java_spring/src/main/java/com/app/rentconnect/v1/mapper/RentalMapper.java
+++ server_java_spring/src/main/java/com/app/rentconnect/v1/mapper/RentalMapper.java
@@ -6,7 +6,7 @@ import com.app.rentconnect.v1.entity.Rental;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",uses = {CarMapper.class, UserMapper.class})
 public interface RentalMapper {
     @Mapping(source = "carId",target = "car.carId")
     Rental toEntity(RentalRequestDTO rentalRequestDTO);
